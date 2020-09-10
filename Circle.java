610af59c90e3b6880610ceeaf3993a8ee009e946
AlbertoCapone077 <70647789+AlbertoCapone077@users.noreply.github.com>
2020-09-10 23:36:47 +0300

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,45 @@
+package shape;
+import java.math.*;
+public class Circle extends Shape{
+    protected double radius;
+    public Circle(){
+        this.Filled = false;
+        this.Color = "blue";
+
+    }
+
+    public Circle(double radius){
+        this.Filled = false;
+        this.Color = "blue";
+        this.radius = radius;
+    }
+    public Circle(double radius, String color, boolean filled){
+        this.radius = radius;
+        this.Color = color;
+        this.Filled = filled;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI*radius*radius;
+
+    }
+
+    @Override
+    public double getPerimeter() {
+
+        return 2*Math.PI*radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape: circle, radius: "+this.radius+", color: "+this.Color+", filled: "+this.Filled;
+
+    }
+}
---
+++ Square.java
@@ -0,0 +1,40 @@
+package shape;
+
+public class Square extends Rectangle
+{
+    public Square() {}
+    public Square(double side){
+        super(side,side);
+    }
+    public Square(double side,String color,boolean filled){
+        super.setLength(side);
+        super.setWidth(side);
+    }
+
+    public double getSide() {
+        return getWidth();
+    }
+
+    public void setSide(double side) {
+        super.setLength(side);
+    }
+
+    @Override
+    public void setWidth(double side) {
+        super.setWidth(width);
+    }
+
+    @Override
+    public void setLength(double side) {
+        super.setLength(length);
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                ", width=" + width +
+                ", length=" + length +
+                ", side=" + this.getSide() +
+                '}';
+    }
+}
